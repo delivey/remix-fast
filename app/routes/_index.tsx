3daa6af2411b517277085add63f57adb6583332b
dev <deliveydev@gmail.com>
2023-11-02 18:42:12 +0200

    No commit message specified

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,28 @@
 import type { MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
+    return [
+        { title: "New Remix App" },
+        { name: "description", content: "Welcome to Remix!" },
+    ];
 };
 
 export default function Index() {
-  return (
-    <div className="bg-black">
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Auth</h1>
+            <ul>
+                <li>
+                    <a target="_blank" href="/login" rel="noreferrer">
+                        Login
+                    </a>
+                </li>
+                <li>
+                    <a target="_blank" href="/signup" rel="noreferrer">
+                        Signup
+                    </a>
+                </li>
+            </ul>
+        </div>
+    );
 }
